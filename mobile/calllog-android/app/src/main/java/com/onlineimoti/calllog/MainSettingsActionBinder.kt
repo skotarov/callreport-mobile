6b81dc8612a721ed5159a3195b313f8a8d43a7b6
skotarov <skotarov@gmail.com>
2026-07-25 19:21:27 +0300

    Wire company list refresh and user controls

--- mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/MainSettingsActionBinder.kt
+++ mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/MainSettingsActionBinder.kt
@@ -32,11 +32,8 @@ internal object MainSettingsActionBinder {
         binding.settingsRegistrationGroup.registrationInviteColleagueButton.setOnClickListener {
             RegistrationActions.showInviteDialog(activity)
         }
-        binding.settingsRegistrationGroup.registrationManageCompaniesButton.setOnClickListener {
-            RegistrationActions.showCompanyManagementDialog(activity) {
-                MainSettingsConfigUi.hydrateServerSettings(binding, ConfigStore.load(activity))
-                RegistrationActions.renderCompanySection(activity, binding.settingsRegistrationGroup)
-            }
+        binding.settingsRegistrationGroup.registrationRefreshCompaniesButton.setOnClickListener {
+            RegistrationActions.renderCompanySection(activity, binding.settingsRegistrationGroup)
         }
         RegistrationActions.renderCompanySection(activity, binding.settingsRegistrationGroup)
         activity.lifecycle.addObserver(object : DefaultLifecycleObserver {
