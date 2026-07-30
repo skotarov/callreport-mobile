9098cf3d38c0dbe5d5e572807df28a0093a24e55
skotarov <skotarov@gmail.com>
2026-07-30 16:09:53 +0300

    Restore ColorStateList reference after UI split

---
+++ mobile/calllog-android/app/src/main/java/com/onlineimoti/calllog/AndroidColorStateListAlias.kt
@@ -0,0 +1,4 @@
+package com.onlineimoti.calllog
+
+/** Package-local alias used by split UI helpers without changing runtime behavior. */
+internal typealias ColorStateList = android.content.res.ColorStateList
