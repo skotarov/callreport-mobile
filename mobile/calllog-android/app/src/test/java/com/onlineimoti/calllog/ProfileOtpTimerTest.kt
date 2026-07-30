6584e9fc0c65f4e35324856a06f0afb93854d0ff
skotarov <skotarov@gmail.com>
2026-07-30 20:52:30 +0300

    Test server-authoritative OTP countdown

--- mobile/calllog-android/app/src/test/java/com/onlineimoti/calllog/ProfileOtpTimerTest.kt
+++ mobile/calllog-android/app/src/test/java/com/onlineimoti/calllog/ProfileOtpTimerTest.kt
@@ -5,9 +5,9 @@ import org.junit.Test
 
 class ProfileOtpTimerTest {
     @Test
-    fun missingServerDeadlineFallsBackToTenMinutes() {
+    fun missingServerDeadlineDoesNotStartLocalCountdown() {
         assertEquals(
-            1_600_000L,
+            0L,
             ProfileOtpTimer.deadline(expiresAtMs = 0L, openedAtMs = 1_000_000L),
         )
     }
@@ -21,12 +21,20 @@ class ProfileOtpTimerTest {
     }
 
     @Test
+    fun expiredServerDeadlineDoesNotStartCountdown() {
+        assertEquals(
+            0L,
+            ProfileOtpTimer.deadline(expiresAtMs = 999_000L, openedAtMs = 1_000_000L),
+        )
+    }
+
+    @Test
     fun expiredCountdownNeverBecomesNegative() {
         assertEquals(0L, ProfileOtpTimer.remainingMs(deadlineMs = 1_000L, nowMs = 2_000L))
     }
 
     @Test
-    fun formatsTenMinuteCountdown() {
+    fun formatsCountdown() {
         assertEquals("10:00", ProfileOtpTimer.format(600_000L))
         assertEquals("09:59", ProfileOtpTimer.format(598_001L))
         assertEquals("00:00", ProfileOtpTimer.format(0L))
